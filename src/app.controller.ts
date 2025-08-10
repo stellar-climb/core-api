df6939d932082dc014bdab6d27fa4fc10f4cf4e3
김정호 <jeangho293@naver.com>
2025-08-10 23:50:48 +0900

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/admins/health')
-  getHello(): string {
+  getHello(@Req() req: Request): string {
+    console.log(req.headers);
     return this.appService.getHello();
   }
 }
