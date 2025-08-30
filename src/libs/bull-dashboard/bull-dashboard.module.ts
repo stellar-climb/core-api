4f7c4f9e43b8ebf09d3f4b8a6975f95203b0c237
김정호 <jeangho293@naver.com>
2025-08-30 14:30:00 +0900

    fix: 동작하는건 확인했음

--- src/libs/bull-dashboard/bull-dashboard.module.ts
+++ src/libs/bull-dashboard/bull-dashboard.module.ts
@@ -3,12 +3,18 @@ import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { BullBoardModule } from '@bull-board/nestjs';
 import { ExpressAdapter } from '@bull-board/express';
 import queues from '../event-box/queues';
+import { AdminGuard } from '../guards';
+import { UsersRepository } from '../../services/users/repository/users.repository';
 
 @Module({
   imports: [
-    BullBoardModule.forRoot({
-      route: '/admins/bull-dashboard',
-      adapter: ExpressAdapter,
+    BullBoardModule.forRootAsync({
+      inject: [UsersRepository],
+      useFactory: () => ({
+        route: '/admins/bull-dashboard',
+        adapter: ExpressAdapter,
+        middleware: [AdminGuard],
+      }),
     }),
     BullBoardModule.forFeature(
       ...queues.map((queue) => ({
